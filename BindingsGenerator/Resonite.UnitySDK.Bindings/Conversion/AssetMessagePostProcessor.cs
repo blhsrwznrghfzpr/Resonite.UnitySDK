0f65d0b2a51968f36a7c89ff212994df3a95a24a
Baplar <me@baplar.fr>
2026-05-15 08:20:42 +0200

    [Post-processing] Allow to specify if the conversion needs to be performed again

--- BindingsGenerator/Resonite.UnitySDK.Bindings/Conversion/AssetMessagePostProcessor.cs
+++ BindingsGenerator/Resonite.UnitySDK.Bindings/Conversion/AssetMessagePostProcessor.cs
@@ -1,1 +1,25 @@
-﻿public delegate void AssetMessagePostProcessor(ResoniteLink.Message message);
+﻿using System;
+
+public abstract class AssetMessagePostProcessor
+{
+    public abstract void Process(ResoniteLink.Message message);
+
+    public abstract bool HasChanged();
+}
+
+public class AssetMessageDelegatePostProcessor : AssetMessagePostProcessor
+{
+    public readonly Action<ResoniteLink.Message> DelegateProcess;
+
+    public AssetMessageDelegatePostProcessor(Action<ResoniteLink.Message> delegateProcess)
+    {
+        DelegateProcess = delegateProcess;
+    }
+
+    public override void Process(ResoniteLink.Message message)
+    {
+        DelegateProcess.Invoke(message);
+    }
+
+    public override bool HasChanged() => false;
+}
